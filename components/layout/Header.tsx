ed1c08114d04379ca297c42e1b399ab35d2e395d
mitsumoto.naoji <mitsumoto.naoji@gmail.com>
2021-08-22 08:28:28 +0900

    add Layout Component

---
+++ components/layout/Header.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+const Header: FC = () => {
+  return <Wrapper>Header</Wrapper>;
+};
+
+export default Header;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 60px;
+`;
