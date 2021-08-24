e548e8a81ba958fb02e4d6d1421ba2a85cce2d30
mitsumoto.naoji <mitsumoto.naoji@gmail.com>
2021-08-25 01:30:03 +0900

    add header logo and modify styles

--- src/components/domains/Store/StoreName.tsx
+++ src/components/domains/Store/StoreName.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import styled from 'styled-components';
 
 import { StoreType } from '@/types/domains/store';
+import MEDIA_QUERY from '@/constants/mediaQuery';
 
 export const StoreName: FC<{ name: StoreType['name'] }> = ({ name }) => {
   return <Wrapper>{name}</Wrapper>;
@@ -14,4 +15,10 @@ const Wrapper = styled.h2`
   justify-content: center;
   width: 200px;
   border-bottom: 2px solid;
+  margin: 50px 0 20px;
+
+  @media ${MEDIA_QUERY.SP} {
+    margin: 20px 0;
+    font-size: 20px;
+  }
 `;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,44 @@
 import { FC } from 'react';
+import Image from 'next/image';
 import styled from 'styled-components';
 
+import logo from '@/assets/common/logo.png';
+import COLORS from '@/constants/colors';
+import MEDIA_QUERY from '@/constants/mediaQuery';
+
 const Header: FC = () => {
-  return <Wrapper>オンライン商店街</Wrapper>;
+  return (
+    <Wrapper>
+      <ImageWrapper>
+        <Image src={logo} placeholder="blur" />
+      </ImageWrapper>
+      <Title>オンライン商店街</Title>
+    </Wrapper>
+  );
 };
 
 export default Header;
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
   height: 60px;
+  padding: 0 40px;
+  box-sizing: border-box;
+  background-color: ${COLORS.WHITE};
+  box-shadow: 0px 0px 15px -5px rgba(0, 0, 0, 0.6);
+
+  @media ${MEDIA_QUERY.SP} {
+    padding: 0 20px;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  width: 30px;
+  height: 30px;
+`;
+
+const Title = styled.h3`
+  margin-left: 10px;
 `;
