d140f73e14cf6339ac062275e4ee7d91fe075626
mitsumoto.naoji <mitsumoto.naoji@gmail.com>
2021-08-26 09:35:43 +0900

    add store background image

--- src/pages/stores/[storeName]/index.tsx
+++ src/pages/stores/[storeName]/index.tsx
@@ -1,28 +1,32 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import styled from 'styled-components';
+
+import { STORES } from '@/assets/stores/stores';
+import background from '@/assets/stores/store_background.png';
 
 import Stack from '@/components/common/Layout/Stack';
 import StoreName from '@/components/domains/Store/StoreName';
 import StoreImage from '@/components/domains/Store/StoreImage';
 import StoreItemList from '@/components/domains/Store/StoreItem/StoreItemList';
-import { STORES } from '@/assets/stores';
-import styled from 'styled-components';
 
 const Store: NextPage = () => {
   const router = useRouter();
   const storeName = router.query.storeName as string;
   const store = fetchStore(storeName);
 
-  if (!store) return <>loading...</>;
+  if (!store) return <></>;
 
   return (
-    <Wrapper>
-      <Stack>
-        <StoreName name={store.name} />
-        <StoreImage image={store.image} links={store.links} />
-        <StoreItemList items={store.items} />
-      </Stack>
-    </Wrapper>
+    <Container>
+      <Wrapper>
+        <Stack>
+          <StoreName name={store.name} />
+          <StoreImage image={store.image} links={store.links} />
+          <StoreItemList items={store.items} />
+        </Stack>
+      </Wrapper>
+    </Container>
   );
 };
 
@@ -31,6 +35,16 @@ const fetchStore = (storeName: string) => {
 };
 
 export default Store;
+
+const Container = styled.div`
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.8),
+      rgba(255, 255, 255, 1),
+      rgba(250, 250, 250, 1),
+      rgba(250, 250, 250, 1)
+    ),
+    url(${background.src}) no-repeat scroll top / contain;
+`;
 
 const Wrapper = styled.div`
   max-width: 800px;
