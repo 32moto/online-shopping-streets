3fbcb8dabfc85e110fc70d7c4c75d3e58f9566df
mitsumoto.naoji <mitsumoto.naoji@gmail.com>
2021-09-04 09:46:09 +0900

    add coco kyoto

--- src/assets/stores/cocoKyoto/index.ts
+++ src/assets/stores/cocoKyoto/index.ts
@@ -1,74 +1,74 @@
-import storeImage from '@/assets/stores/minifilm/storeImage/minifilm_image.png';
-import minifilmItem1 from '@/assets/stores/minifilm/itemImages/minifilm_item_1.jpeg';
-import minifilmItem2 from '@/assets/stores/minifilm/itemImages/minifilm_item_2.jpeg';
-import minifilmItem3 from '@/assets/stores/minifilm/itemImages/minifilm_item_3.jpeg';
-import minifilmItem4 from '@/assets/stores/minifilm/itemImages/minifilm_item_4.jpeg';
-import minifilmItem5 from '@/assets/stores/minifilm/itemImages/minifilm_item_5.jpeg';
-import minifilmItem6 from '@/assets/stores/minifilm/itemImages/minifilm_item_6.jpeg';
-import minifilmItem7 from '@/assets/stores/minifilm/itemImages/minifilm_item_7.jpeg';
-import minifilmItem8 from '@/assets/stores/minifilm/itemImages/minifilm_item_8.jpeg';
+import storeImage from '@/assets/stores/cocoKyoto/storeImage/coco_kyoto_image.png';
+import cocoKyotoItem1 from '@/assets/stores/cocoKyoto/itemImages/coco_kyoto_item_1.jpg';
+import cocoKyotoItem2 from '@/assets/stores/cocoKyoto/itemImages/coco_kyoto_item_2.jpeg';
+import cocoKyotoItem3 from '@/assets/stores/cocoKyoto/itemImages/coco_kyoto_item_3.jpeg';
+import cocoKyotoItem4 from '@/assets/stores/cocoKyoto/itemImages/coco_kyoto_item_4.jpeg';
+import cocoKyotoItem5 from '@/assets/stores/cocoKyoto/itemImages/coco_kyoto_item_5.jpeg';
+import cocoKyotoItem6 from '@/assets/stores/cocoKyoto/itemImages/coco_kyoto_item_6.jpeg';
+import cocoKyotoItem7 from '@/assets/stores/cocoKyoto/itemImages/coco_kyoto_item_7.jpeg';
+import cocoKyotoItem8 from '@/assets/stores/cocoKyoto/itemImages/coco_kyoto_item_8.jpeg';
 
 const LINKS = [
   {
     title: 'オンラインストアへ',
-    url: 'https://minifilm.base.shop/',
+    url: 'https://shop.coco-cacao.jp/',
     right: 80,
     top: 250,
   },
   {
-    title: 'Minifilm について',
-    url: 'https://minifilm.base.shop/about',
+    title: 'COCO KYOTO について',
+    url: 'https://shop.coco-cacao.jp/about',
     left: 60,
     top: 300,
   },
   {
-    title: 'Youtube Channel',
-    url: 'https://www.youtube.com/c/TsukaTsukaMinifilm',
+    title: 'COCO KYOTO ブログ',
+    url: 'https://shop.coco-cacao.jp/blog',
     right: 170,
-    bottom: 150,
+    bottom: 100,
   },
 ];
 
 const ITEMS = [
   {
-    name: 'バターサンド4種詰め合わせ　8個入り　【9月限定】',
-    url: 'https://minifilm.base.shop/items/51441281',
-    image: minifilmItem1,
+    name: '商品を購入頂く前にご確認ください',
+    url: 'https://shop.coco-cacao.jp/items/27547025',
+    image: cocoKyotoItem1,
   },
   {
-    name: 'フロマージュキュイ【ベイクドチーズケーキ】',
-    url: 'https://minifilm.base.shop/items/50523020',
-    image: minifilmItem2,
+    name: 'マドレーヌレモン6個入',
+    url: 'https://shop.coco-cacao.jp/items/43468952',
+    image: cocoKyotoItem2,
   },
   {
-    name: 'レーズンバターサンドショコラ　6個入り',
-    url: 'https://minifilm.base.shop/items/49076521',
-    image: minifilmItem3,
+    name: 'Chocolate Terrine 抹茶',
+    url: 'https://shop.coco-cacao.jp/items/13260869',
+    image: cocoKyotoItem3,
   },
   {
-    name: 'ライチフランボワーズバターサンド　6個入り　【夏季限定】',
-    url: 'https://minifilm.base.shop/items/47661506',
-    image: minifilmItem4,
+    name: 'ＣＯＣＯチョコロールケーキ',
+    url: 'https://shop.coco-cacao.jp/items/21043630',
+    image: cocoKyotoItem4,
   },
   {
-    name: 'プラリネバターサンド ６個入り',
-    url: 'https://minifilm.base.shop/items/44425982',
-    image: minifilmItem5,
+    name: 'シュトロイゼルガトーショコラ',
+    url: 'https://shop.coco-cacao.jp/items/26977513',
+    image: cocoKyotoItem5,
   },
   {
-    name: 'レーズンバターサンド６個入り',
-    url: 'https://minifilm.base.shop/items/42136282',
-    image: minifilmItem6,
+    name: 'Chocolate Terrine',
+    url: 'https://shop.coco-cacao.jp/items/12387610',
+    image: cocoKyotoItem6,
   },
   {
-    name: 'ケークフリュイルージュ　【パウンドケーキ　ベリー】',
-    url: 'https://minifilm.base.shop/items/46359130',
-    image: minifilmItem7,
+    name: '京味　詰合4種',
+    url: 'https://shop.coco-cacao.jp/items/34609052',
+    image: cocoKyotoItem7,
   },
   {
-    name: 'フィナンシエ 単品',
-    url: 'https://minifilm.base.shop/items/43964273',
-    image: minifilmItem8,
+    name: 'Brownie【くるみ】5個入り',
+    url: 'https://shop.coco-cacao.jp/items/37687166',
+    image: cocoKyotoItem8,
   },
 ];
 
