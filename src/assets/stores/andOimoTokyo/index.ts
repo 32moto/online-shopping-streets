a5279a9041effacb40886304896575d6b26960ea
mitsumoto.naoji <mitsumoto.naoji@gmail.com>
2021-09-04 10:28:31 +0900

    add and oimo tokyo

--- src/assets/stores/andOimoTokyo/index.ts
+++ src/assets/stores/andOimoTokyo/index.ts
@@ -1,74 +1,38 @@
-import storeImage from '@/assets/stores/minifilm/storeImage/minifilm_image.png';
-import minifilmItem1 from '@/assets/stores/minifilm/itemImages/minifilm_item_1.jpeg';
-import minifilmItem2 from '@/assets/stores/minifilm/itemImages/minifilm_item_2.jpeg';
-import minifilmItem3 from '@/assets/stores/minifilm/itemImages/minifilm_item_3.jpeg';
-import minifilmItem4 from '@/assets/stores/minifilm/itemImages/minifilm_item_4.jpeg';
-import minifilmItem5 from '@/assets/stores/minifilm/itemImages/minifilm_item_5.jpeg';
-import minifilmItem6 from '@/assets/stores/minifilm/itemImages/minifilm_item_6.jpeg';
-import minifilmItem7 from '@/assets/stores/minifilm/itemImages/minifilm_item_7.jpeg';
-import minifilmItem8 from '@/assets/stores/minifilm/itemImages/minifilm_item_8.jpeg';
+import storeImage from '@/assets/stores/andOimoTokyo/storeImage/and_oimo_tokyo_image.png';
+import item1 from '@/assets/stores/andOimoTokyo/itemImages/and_oimo_tokyo_item_1.png';
+import item2 from '@/assets/stores/andOimoTokyo/itemImages/and_oimo_tokyo_item_2.png';
 
 const LINKS = [
   {
     title: 'オンラインストアへ',
-    url: 'https://minifilm.base.shop/',
+    url: 'https://www.and-oimo-tokyo.com/',
     right: 80,
     top: 250,
   },
   {
-    title: 'Minifilm について',
-    url: 'https://minifilm.base.shop/about',
-    left: 60,
+    title: 'News',
+    url: 'https://www.and-oimo-tokyo.com/news',
+    left: 80,
     top: 300,
   },
   {
-    title: 'Youtube Channel',
-    url: 'https://www.youtube.com/c/TsukaTsukaMinifilm',
+    title: 'Instagram',
+    url: 'https://www.instagram.com/and_oimo_tokyo/',
     right: 170,
-    bottom: 150,
+    bottom: 100,
   },
 ];
 
 const ITEMS = [
   {
-    name: 'バターサンド4種詰め合わせ　8個入り　【9月限定】',
-    url: 'https://minifilm.base.shop/items/51441281',
-    image: minifilmItem1,
+    name: '蜜芋バスクチーズケーキ',
+    url: 'https://www.and-oimo-tokyo.com/product-page/mitsuimobasque',
+    image: item1,
   },
   {
-    name: 'フロマージュキュイ【ベイクドチーズケーキ】',
-    url: 'https://minifilm.base.shop/items/50523020',
-    image: minifilmItem2,
-  },
-  {
-    name: 'レーズンバターサンドショコラ　6個入り',
-    url: 'https://minifilm.base.shop/items/49076521',
-    image: minifilmItem3,
-  },
-  {
-    name: 'ライチフランボワーズバターサンド　6個入り　【夏季限定】',
-    url: 'https://minifilm.base.shop/items/47661506',
-    image: minifilmItem4,
-  },
-  {
-    name: 'プラリネバターサンド ６個入り',
-    url: 'https://minifilm.base.shop/items/44425982',
-    image: minifilmItem5,
-  },
-  {
-    name: 'レーズンバターサンド６個入り',
-    url: 'https://minifilm.base.shop/items/42136282',
-    image: minifilmItem6,
-  },
-  {
-    name: 'ケークフリュイルージュ　【パウンドケーキ　ベリー】',
-    url: 'https://minifilm.base.shop/items/46359130',
-    image: minifilmItem7,
-  },
-  {
-    name: 'フィナンシエ 単品',
-    url: 'https://minifilm.base.shop/items/43964273',
-    image: minifilmItem8,
+    name: '蜜芋バスクチーズケーキ（ギフトラッピング）',
+    url: 'https://www.and-oimo-tokyo.com/product-page/mitsuimobasque-gift',
+    image: item2,
   },
 ];
 
