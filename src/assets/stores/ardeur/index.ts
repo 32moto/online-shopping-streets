10e6ec1207e1ab35851ab8e210f7b88ddf545def
mitsumoto.naoji <mitsumoto.naoji@gmail.com>
2021-09-04 11:11:30 +0900

    modify ardeur image

--- src/assets/stores/ardeur/index.ts
+++ src/assets/stores/ardeur/index.ts
@@ -1,12 +1,12 @@
 import storeImage from '@/assets/stores/ardeur/storeImage/ardeur.png';
-import ardeurItem1 from '@/assets/stores/ardeur/itemImages/ardeur_item_1.jpeg';
-import ardeurItem2 from '@/assets/stores/ardeur/itemImages/ardeur_item_2.jpeg';
-import ardeurItem3 from '@/assets/stores/ardeur/itemImages/ardeur_item_3.jpeg';
-import ardeurItem4 from '@/assets/stores/ardeur/itemImages/ardeur_item_4.jpeg';
-import ardeurItem5 from '@/assets/stores/ardeur/itemImages/ardeur_item_5.jpeg';
-import ardeurItem6 from '@/assets/stores/ardeur/itemImages/ardeur_item_6.jpeg';
-import ardeurItem7 from '@/assets/stores/ardeur/itemImages/ardeur_item_7.jpeg';
-import ardeurItem8 from '@/assets/stores/ardeur/itemImages/ardeur_item_8.jpeg';
+import item1 from '@/assets/stores/ardeur/itemImages/ardeur_item_1.jpeg';
+import item2 from '@/assets/stores/ardeur/itemImages/ardeur_item_2.jpg';
+import item3 from '@/assets/stores/ardeur/itemImages/ardeur_item_3.jpeg';
+import item4 from '@/assets/stores/ardeur/itemImages/ardeur_item_4.jpeg';
+import item5 from '@/assets/stores/ardeur/itemImages/ardeur_item_5.jpeg';
+import item6 from '@/assets/stores/ardeur/itemImages/ardeur_item_6.jpeg';
+import item7 from '@/assets/stores/ardeur/itemImages/ardeur_item_7.jpg';
+import item8 from '@/assets/stores/ardeur/itemImages/ardeur_item_8.jpg';
 
 const LINKS = [
   {
@@ -33,42 +33,42 @@ const ITEMS = [
   {
     name: 'マカロン5個入',
     url: 'https://ardeur.stores.jp/items/5ece0e0a72b911625e3ebf58',
-    image: ardeurItem1,
+    image: item1,
   },
   {
     name: 'マカロン12個入セット',
     url: 'https://ardeur.stores.jp/items/5f02e878ec8fd351a639613e',
-    image: ardeurItem2,
+    image: item2,
   },
   {
     name: 'マカロン16個入セット',
     url: 'https://ardeur.stores.jp/items/5ece0e7455fa0360b0efb4a5',
-    image: ardeurItem3,
+    image: item3,
   },
   {
     name: 'ケーク･オ･コニャック',
     url: 'https://ardeur.stores.jp/items/60c9694b103986044cbd1182',
-    image: ardeurItem4,
+    image: item4,
   },
   {
     name: 'ケーク･オ･テヴェール',
     url: 'https://ardeur.stores.jp/items/60c9687391260a7b217997f5',
-    image: ardeurItem5,
+    image: item5,
   },
   {
     name: 'ケーク･オ･ショコラ',
     url: 'https://ardeur.stores.jp/items/60c9679ad7e1d862357af71d',
-    image: ardeurItem6,
+    image: item6,
   },
   {
     name: '焼菓子8個詰め合わせ',
     url: 'https://ardeur.stores.jp/items/5ece0dfb72b911628e3ebefa',
-    image: ardeurItem7,
+    image: item7,
   },
   {
     name: '焼菓子16個詰め合わせ',
     url: 'https://ardeur.stores.jp/items/5ece0ded5157622946174e8d',
-    image: ardeurItem8,
+    image: item8,
   },
 ];
 
